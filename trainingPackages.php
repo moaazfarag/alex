a75fea42e3a2752331eef5f990617e644edc04b8
mohamed hafez <mohamed.hafezqo@gmail.com>
2015-03-25 11:43:55 +0200

    just change in trainingPackges.php in design to draw trainingPackges.html

--- trainingPackages.php
+++ trainingPackages.php
@@ -8,22 +8,4 @@
 
 require_once('globals.php');
 
-require_once 'globals.php';
-require_once (CONTROLLERS.'allController.php');
-is_admin();
-
-/*
- * objects from Visit Request Model and users Model
- * 
- */
-$usersmodel     = new usersModel();
-/*
- * controller instance
- */
-$controller     = new usersController ($usersmodel);
-
-$controller->getAllUsers();
-
-//is_admin();
-//System::get('tpl')->assign('username',$_SESSION['username']);
 System::Get('tpl')->draw('trainingPackages');
